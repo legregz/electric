74cb91181fc3402d4ef94b3212ff4c17d2497eb2
legregz <gregggmar01@gmail.com>
2024-10-10 08:00:51 +0200

    💄 update UI

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame, pyperclip
 pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((1050, 1050))
 pygame.display.set_caption("Electric")
 
 run = True
@@ -13,26 +13,28 @@ class Matrix:
     def show(self):
         for y in range(len(self.matrix)):
             for x in range(len(self.matrix[y])):
-                if self.matrix[y][x] == 1:
-                    if x % 2 == 0 and y % 2 == 0:
+                if self.matrix[y][x]:
+                    if x % 2 == 1 and y % 2 == 1:
                         if x > 0:
                             if self.matrix[y][x - 1]:
-                                pygame.draw.rect(screen, (255, 255, 255), (x * 50, y * 50 + 15, 15, 20))
+                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 - 25, y * 50 + 15, 50, 20))
 
                         if x < len(self.matrix[y]) - 1:
                             if self.matrix[y][x + 1]:
-                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 35, y * 50 + 15, 15, 20))
+                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 25, y * 50 + 15, 50, 20))
 
                         if y > 0:
                             if self.matrix[y - 1][x]:
-                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50, 20, 15))
+                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50 - 25, 20, 50))
 
                         if y < len(self.matrix) - 1:
                             if self.matrix[y + 1][x]:
-                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50 + 35, 20, 15))
-                        pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50 + 15, 20, 20))
-                    else:
-                        pygame.draw.rect(screen, (255, 0, 0), (x * 50 + 15, y * 50 + 15, 20, 20))
+                                pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50 + 25, 20, 50))
+                                
+                        #pygame.draw.rect(screen, (255, 255, 255), (x * 50 + 15, y * 50 + 15, 20, 20))
+                        pygame.draw.circle(screen, (255, 255, 255), (x * 50 + 25, y * 50 + 25), 10)
+                    """else:
+                        pygame.draw.rect(screen, (255, 0, 0), (x * 50 + 15, y * 50 + 15, 20, 20))"""
 
     def get(self, x, y):
         return self.matrix[y][x]
@@ -42,8 +44,8 @@ class Matrix:
 
     def export(self):
         string = ""
-        for y in range(0, len(self.matrix), 2):
-            for x in range(0, len(self.matrix[y]), 2):
+        for y in range(1, len(self.matrix), 2):
+            for x in range(1, len(self.matrix[y]), 2):
                 if self.matrix[y][x] == 1:
                     value = 0
 
@@ -66,16 +68,13 @@ class Matrix:
                     if value == 0:
                         string += " "
                     if value == 1:
-                    #    string += "╶"
-                        string += " "
+                        string += "╶"
                     if value == 2:
-                    #    string += "╴"
-                        string += " "
+                        string += "╴"
                     if value == 3:
                         string += "―"
                     if value == 4:
-                    #    string += "╷"
-                        string += " "
+                        string += "╷"
                     if value == 5: 
                         string += "┌"
                     if value == 6:
@@ -83,8 +82,7 @@ class Matrix:
                     if value == 7:
                         string += "┬"
                     if value == 8:
-                    #    string += "╵"
-                        string += " "
+                        string += "╵"
                     if value == 9:
                         string += "└"
                     if value == 10:
@@ -106,7 +104,7 @@ class Matrix:
             string += "\n"
         pyperclip.copy(string)
 
-matrix = Matrix([16, 16])
+matrix = Matrix([21, 21])
 
 while run:
     for event in pygame.event.get():
@@ -122,18 +120,24 @@ while run:
                 x, y = pygame.mouse.get_pos()
                 x //= 50
                 y //= 50
-                if matrix.get(x, y) == 1:
+                if matrix.get(x, y):
                     matrix.set(x, y, 0)
                 else:
                     matrix.set(x, y, 1)
 
-    screen.fill((0, 0, 0))
+    screen.fill((4, 3, 72))
 
-    for y in range(0, 800, 50):
-        pygame.draw.line(screen, (255, 255, 255), (0, y), (800, y))
+    for y in range(0, screen.get_width(), 50):
+        if y % 100 == 0:
+            pygame.draw.line(screen, (0, 0, 150), (25, y + 25), (screen.get_width() - 25, y + 25))
+        else:
+            pygame.draw.line(screen, (200, 200, 200), (25, y + 25), (screen.get_width() - 25, y + 25))
 
-    for x in range(0, 800, 50):
-        pygame.draw.line(screen, (255, 255, 255), (x, 0), (x, 800))
+    for x in range(0, screen.get_height(), 50):
+        if x % 100 == 0:
+            pygame.draw.line(screen, (0, 0, 150), (x + 25, 25), (x + 25, screen.get_height() - 25))
+        else:
+            pygame.draw.line(screen, (200, 200, 200), (x + 25, 25), (x + 25, screen.get_height() - 25))
 
     matrix.show()
 
